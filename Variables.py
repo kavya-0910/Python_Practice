752d2366cd65603d35ac77e65ab4fe3babf4c329
kavya-0910 <nameisnani2209@gmail.com>
2026-06-07 00:35:14 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,37 @@
+"""Variables"""
+student_name="Nani"
+student_age=21
+student_course="Python"
+student_marks=95
+print(student_name)
+print(student_age)
+print(student_course)
+print(student_marks)
+print("-"*25)
+product_name = "keerthy"
+product_id = 2209
+product_price = 1000000000000000
+product_brand = "Badugu"
+print(product_name)
+print(product_id)
+print(product_price)
+print(product_brand)
+print("-"*25)
+person_name = "Nani"
+person_age = 30
+person_city = "vijayawada"
+print(person_name)
+print(person_age)
+print(person_city)
+print(type(person_name))
+print(type(person_age))
+print(type(person_city))
+print("-"*25)
+value_1 = 10
+value_2 = 20
+print("Value_1: ",value_1)
+print("value_2: ",value_2)
+value_1,value_2 =value_2,value_1
+print("swapping")
+print("value_1: ",value_1)
+print("value_2: ",value_2)
