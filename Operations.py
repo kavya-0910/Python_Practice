bbfec9aaaba6ff454e3ca5eca9b6f0e3fb67ee83
kavya-0910 <nameisnani2209@gmail.com>
2026-06-08 23:02:20 +0530

    Create Operations.py

---
+++ Operations.py
@@ -0,0 +1,16 @@
+num_1 = 22
+num_2 = 9
+sum = num_1+num_2
+print("Sum of two numbers:",sum)
+sub = num_1-num_2
+print("Subtraction of two numbers:",sub)
+mul = num_1*num_2
+print("Multiplication of two number:",mul)
+div = num_1/num_2
+print("Division of two numbers:",div)
+exp = num_1**num_2
+print("Exponent of two numbers:",exp)
+mod = num_1%num_2
+print("Modulus of two numbers:",mod)
+floor_div = num_1//num_2
+print("Floor Division of two numbers:",floor_div)
