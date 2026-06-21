efd8c483aa6a3e7629b8accce5f8e2649a932ec6
kavya-0910 <nameisnani2209@gmail.com>
2026-06-21 21:58:56 +0530

    Fix pattern printing logic in patterns.py

---
+++ patterns.py
@@ -0,0 +1,11 @@
+for row in range(1,6):
+    for col in range(1,6):
+        print("*",end=" ")
+    print()
+print("----")
+counter=1
+for row in range(1,6):
+    for col in range(1,6):
+        print(counter,end=" ")
+        counter+=1
+    print()
